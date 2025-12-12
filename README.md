64f8a1d46911369792ba044ecbd550def44f857a
C Henrique <henriquecamis.ch@gmail.com>
2025-12-12 18:04:54 -0300

    aula 6

---
+++ README.md
@@ -0,0 +1,3 @@
+## Cursinho em GO
+https://youtu.be/WSK0mnO1o8U?si=ewbIdPBN153yOICm
+parei na aula 6
--- src/controller/findUser.go
+++ src/controller/findUser.go
@@ -1,9 +1,14 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/CHenrique-Oliv/estudo-go/src/config/rest_err"
+	"github.com/gin-gonic/gin"
+)
 
 func FindUserById(c *gin.Context) {
 
+	err := rest_err.NewBadRequestError("Erro de teste")
+	c.JSON(err.Code, err)
 }
 
 func FindUserByEmail(c *gin.Context) {
